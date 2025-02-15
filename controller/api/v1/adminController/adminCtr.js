5d51b24b40f8f9e2248ad561a8a6703091ea2411
mitulbhimani07 <mitulbhimani281@gmail.com>
2025-02-15 19:19:50 +0530

    admin update

--- controller/api/v1/adminController/adminCtr.js
+++ controller/api/v1/adminController/adminCtr.js
@@ -2,6 +2,8 @@
 const signin = require('../../../../model/adminModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const nodemailer = require('nodemailer')
+
 module.exports.home = async (req,res)=>{
     try{
         let getuser = await signin.find()
@@ -123,6 +125,16 @@ module.exports.adminprofile = async (req,res) =>{
 
     }
 }
+module.exports.FindUpdateData = async (req,res)=>{
+    try{
+        let findupdate = await signin.findById(req.params.id);
+        return  res.status(200).json({msg:"Updated Page",data:findupdate});
+    }
+    catch(err){
+        console.log(err);
+        return  res.status(200).json({msg:"somthing woas wrong !"});
+    }
+}
 module.exports.update = async (req,res) =>{
     console.log(req.params.id);
     
@@ -148,3 +160,93 @@ module.exports.update = async (req,res) =>{
 
     }
 }
+module.exports.ChangePassword = async (req, res) => {
+    try {      
+        let AdminData = await signin.findById(req.user._id)
+        console.log(AdminData);
+        if (AdminData) {
+            let checkPassword = await bcrypt.compare(req.body.currentpassword, AdminData.password)
+            if (checkPassword) {
+                if (req.body.currentpassword != req.body.newPassword) {
+                    if (req.body.newPassword == req.body.confirmPassword) {
+                        req.body.newPassword = bcrypt.hash(req.body.confirmPassword, 10)
+                        let newData = await signin.findByIdAndUpdate(req.user._id, req.body)
+                        if (newData) {
+                            let data = await signin.findById(req.user._id)
+                            return res.status(200).json({ mes: "password change success", Data: data })
+                        } else {
+                            return res.status(200).json({ mes: "password change faild" })
+                        }
+                    } else {
+                        return res.status(200).json({ mes: "new password and confirm password is not same" })
+                    }
+                } else {
+                    return res.status(200).json({ mes: "new password and current password is same" })
+                }
+            } else {
+                return res.status(200).json({ mes: "your current password is wrong" })
+            }
+        } else {
+            return res.status(200).json({ mes: "Record Not Found" })
+        }
+    } catch (err) {
+        return res.status(400).json({ mes: "Somthing Wrong" })
+    }
+}
+module.exports.forgetPassword = async (req, res) => {
+    console.log(req.body);
+  
+    try {
+      // 1. Check if user exists
+      let isAdmin = await signin.findOne({ email: req.body.email });
+      if (!isAdmin) {
+        return res.status(400).json({ mes: "Invalid email" });
+      }
+  
+      // 2. Generate a 6-digit OTP
+      const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  
+      // 3. Store OTP and expiry in the database (valid for 15 minutes here)
+      isAdmin.otp = otp;
+      isAdmin.otpExpiry = Date.now() + 15 * 60 * 1000; // 15 minutes from now
+      await isAdmin.save();
+  
+      // 4. Create the transporter
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "mitulbhimani281@gmail.com",  // your email
+          pass: "uugochxrfxrylbjw",           // your app password
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
+  
+      // 5. Send the OTP via email
+      const info = await transporter.sendMail({
+        from: `${req.body.email}`, // or your email
+        to: "mitulbhimani281@gmail.com",   // or req.body.email to send OTP to the user
+        subject: "Password Reset OTP",
+        text: `Your OTP is: ${otp}`,       // plain text body
+        html: `
+            <h3>Password Reset Request</h3>
+            <p>Your OTP is: <b>${otp}</b></p>
+            <p>This OTP will expire in 15 minutes.</p>
+            <p>Please click the link below to proceed with your password reset:</p>
+            <p><a href="http://localhost:8005/api/changePassword">Reset Password</a></p>
+            <p>If you did not request a password reset, please ignore this email.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            `
+        ,
+      });
+  
+      return res.status(200).json({ mes: "Password reset OTP sent to your email." });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ mes: "Something went wrong" });
+    }
+};
+  
--- routes/api/v1/adminRoutes/index.js
+++ routes/api/v1/adminRoutes/index.js
@@ -3,12 +3,15 @@ const routes = express.Router();
 const Adminctr = require('../../../../controller/api/v1/adminController/adminCtr')
 const passport = require('passport')
 
-routes.get('/', passport.authenticate('jwt', { failureRedirect: '/unauthor' }),Adminctr.home);
-routes.post('/signin', passport.authenticate('jwt', { failureRedirect: '/unauthor' }),Adminctr.signin)
+routes.get('/', Adminctr.home);
+routes.post('/signin', Adminctr.signin)
 routes.post('/inserData', passport.authenticate('jwt', { failureRedirect: '/unauthor' }),Adminctr.singleSignUp);
 routes.get('/statusData',passport.authenticate('jwt', { failureRedirect: '/unauthor' }),Adminctr.singledata)
 routes.post('/adminprofile',passport.authenticate('jwt', { failureRedirect: '/unauthor' }),Adminctr.adminprofile)
 routes.put('/update/:id',passport.authenticate('jwt', { failureRedirect: '/unauthor' }),Adminctr.update)
+routes.get('/FindUpdateData/:id',Adminctr.FindUpdateData)
+routes.patch('/changePassword' ,passport.authenticate('jwt', { failureRedirect: '/unauthor' }) ,  Adminctr.ChangePassword)
+routes.post('/forgetpassword', Adminctr.forgetPassword)
 routes.get('/unauthor', (req, res) => {
     try {
         return res.status(200).json({ msg: "your authorized" });
@@ -19,6 +22,5 @@ routes.get('/unauthor', (req, res) => {
         });
     }
 });
-
 
 module.exports = routes;
